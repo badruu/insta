cf86a35fbdb6998811c2febd02e5d103fe53146a
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-17 18:29:55 +0300

    add urls for password reset via email

--- insta/urls.py
+++ insta/urls.py
@@ -23,14 +23,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('instagram.urls')),
-    url(r'^', include('django.contrib.auth.urls')),
     url(r'register/', user_views.register, name='register'),
     url(r'profile/', user_views.profile, name='profile'),
     url(r'login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    url(r'password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    url(r'password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
     url(r'password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    url(r'password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    url(r'password-reset-confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     url(r'password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 if settings.DEBUG:
