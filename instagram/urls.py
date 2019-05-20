294011a0ebbe324b7282b7f7e9bc06de7848d8eb
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-20 14:14:14 +0300

    add url for user posts

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ImageCreateView,ImageListView,ImageDetailView,UserImageListView,ImageUpdateView,ImageDeleteView
-
+from .views import ImageCreateView,ImageListView,ImageDetailView,UserImageListView,ImageUpdateView,ImageDeleteView,CommentCreateView
 
 
 urlpatterns=[
     url(r'^$', ImageListView.as_view(), name = 'instagram-home'),
-    # url(r'^user/(?P<username>\d+)/$',UserImageListView.as_view(), name='user-posts'),
+    # url(r'^user/(?P<username>\w+)/$',UserImageListView.as_view(), name='user-posts'),
+    # url(r'^post/(?P<username>\w+)/$',UserImageListView.as_view(), name='user-posts'),
+    url(r'^post/(?P<username>\w+)/$', UserImageListView.as_view(), name='user-posts'),
     url(r'^post/new/$',ImageCreateView.as_view(), name='image-create'),
+    url(r'^post/(?P<pk>\d+)/comment/$',CommentCreateView.as_view(), name='comment-create'),
     url(r'^image/(?P<pk>\d+)/',ImageDetailView.as_view(), name='image-detail'),
     url(r'^about/', views.about, name = 'instagram-about'),
     url(r'^post/(?P<pk>\d+)/update/',ImageUpdateView.as_view(), name='image-update'),
