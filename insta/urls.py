915226329fc3007e26decb39f6388954fead3dec
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-23 14:34:45 +0300

    add a url for comments

--- insta/urls.py
+++ insta/urls.py
@@ -19,10 +19,12 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from users.views import ProfileDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('instagram.urls')),
+    url(r'^comments/', include('django_comments.urls')),
     url(r'register/', user_views.register, name='register'),
     url(r'profile/', user_views.profile, name='profile'),
     url(r'login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
@@ -31,6 +33,7 @@ urlpatterns = [
     url(r'password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     url(r'password-reset-confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     url(r'password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    url(r'^user/(?P<pk>\d+)/',ProfileDetailView.as_view(), name='profile-detail'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
